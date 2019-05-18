80e280d81ac2e85913113f230fdd09ec4d7b4463
hidetoshitsubaki <hidetoshi.tsubaki@gmail.com>
2019-05-19 02:46:00 +0900

    edit model news

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,4 @@
 class News < ApplicationRecord
-  validates :eyecatch,:title, :contents presence: true
-
-   enum tag: { stuby_in_japan: 1, lifestyle: 2, news: 3, work_in_japan: 4}
+  validates :eyecatch,:title,:contents ,presence: true
+  # enum tag: { stuby_in_japan: 1, lifestyle: 2, news: 3, work_in_japan: 4}
 end
