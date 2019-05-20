2b024d2eb0fa59ecaf5d16af9b8e0ad706a630d8
hidetoshitsubaki <hidetoshi.tsubaki@gmail.com>
2019-05-20 22:22:05 +0900

    change mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
@@ -107,7 +107,7 @@ Rails.application.configure do
     :domain => ENV["DOMAIN"],
     :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
-    :authentication => :plain,
+    :authentication => :login,
     :enable_starttls_auto => true
   }
 end
