2b1e037a626739df37c4e21dc417dff17abf960e
hidetoshitsubaki <hidetoshi.tsubaki@gmail.com>
2019-05-27 15:48:26 +0900

    add ajax form to talks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190521053456) do
+ActiveRecord::Schema.define(version: 20190524053715) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,25 @@ ActiveRecord::Schema.define(version: 20190521053456) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
- 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "controllers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "admins"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_controllers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
+  end
 
   create_table "mistakes", force: :cascade do |t|
     t.integer "user_id"
