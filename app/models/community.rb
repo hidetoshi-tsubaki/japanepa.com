2c9209231983f7815b308fd07871804dd2a3e940
hidetoshitsubaki <hidetoshi.tsubaki@gmail.com>
2020-05-30 23:20:53 +0900

    feature/#68/202005/add top 3 ranking to community page

--- app/models/community.rb
+++ app/models/community.rb
@@ -6,9 +6,10 @@ class Community < ApplicationRecord
   has_many :talks, dependent: :destroy
   belongs_to :founder, class_name: 'User', foreign_key: 'founder_id'
   has_one_attached :img
-  # validate :img_presence
   validate :image_content_type, if: :was_attached?
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, :introduction, :founder_id, presence: true
 
+  scope :user_joined_top_3, -> { with_attached_img.order(member_count: :desc).limit(3) }
+  scope :active_top_3, -> { with_attached_img.order(talk_count: :desc).limit(3) }
 end
